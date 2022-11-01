be00794d48ab1a19a24a4730a8a59e2026f576e8
solm7 <105757874+solm7@users.noreply.github.com>
2022-10-31 21:00:03 -0400

    user js

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: [true, "Enter Username."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Enter email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
