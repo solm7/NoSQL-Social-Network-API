8502a6e31d918b3c533bd47b5bdfb869f0a5cc83
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-05 16:52:01 -0400

    added controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,114 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .select("-__v")
+      .then((UserData) => {
+        res.json(UserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // get single user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate({
+        select: "-__v",
+        path: "thoughts",
+        path: "friends",
+      })
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "No user found with associated ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createNewUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // delete user associated thoughts
+  removeUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID" })
+          : Thought.findOneAndUpdate(
+              { userId: req.params.userId },
+              { $pull: { thoughts: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "User removed but no thoughts found.",
+            })
+          : res.json({ message: "User successfully removed" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // add friend
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // remove friend from friend list
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
+module.exports = userController;
